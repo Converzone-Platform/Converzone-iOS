070c6470aec753d4fd75ee3ad62b3fd469cdaab5
Goga Barabadze <goga.barabadze73@gmail.com>
2020-10-15 16:07:49 +0200

    Changed Development Target to iOS 13.0

--- converzone/View/MainTBC.swift
+++ converzone/View/MainTBC.swift
@@ -15,20 +15,10 @@ class MainTBC: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if #available(iOS 13.0, *) {
-            
-            let configuration = UIImage.SymbolConfiguration(pointSize: 17, weight: .medium)
-            
-            tab_bar.items?[0].image = UIImage(systemName: "ellipses.bubble", withConfiguration: configuration)
-            tab_bar.items?[1].image = UIImage(systemName: "globe", withConfiguration: UIImage.SymbolConfiguration(pointSize: 17, weight: .medium))
-            tab_bar.items?[2].image = UIImage(systemName: "rectangle.stack.person.crop", withConfiguration: configuration)
-            
-        }else{
-            
-            tab_bar.items?[0].image = UIImage(named: "chat_nav")
-            tab_bar.items?[1].image = UIImage(named: "web_nav")
-            tab_bar.items?[2].image = UIImage(named: "discover_nav")
-            
-        }
+        let configuration = UIImage.SymbolConfiguration(pointSize: 17, weight: .medium)
+        
+        tab_bar.items?[0].image = UIImage(systemName: "ellipses.bubble", withConfiguration: configuration)
+        tab_bar.items?[1].image = UIImage(systemName: "globe", withConfiguration: UIImage.SymbolConfiguration(pointSize: 17, weight: .medium))
+        tab_bar.items?[2].image = UIImage(systemName: "rectangle.stack.person.crop", withConfiguration: configuration)
     }
 }
